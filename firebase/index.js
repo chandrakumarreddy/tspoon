a701d4d550e337b2c98830def37aa0fc2b456ad7
Chandra <chandrapenugonda655@gmail.com>
2020-06-03 11:14:33 +0530

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCxkOw34iOQSMW4HCzTZtqpGfU_wG6KByk",
-  authDomain: "fir-d1aa9.firebaseapp.com",
-  databaseURL: "https://fir-d1aa9.firebaseio.com",
-  projectId: "fir-d1aa9",
-  storageBucket: "fir-d1aa9.appspot.com",
-  messagingSenderId: "1070843129594",
-  appId: "1:1070843129594:web:b60aa7bbb583152bef2343",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 if (!firebase.apps.length) {
